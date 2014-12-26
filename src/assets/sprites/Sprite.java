b26936fa7d3ca1812d3541dcaf38c13645b169d4
Saejin <Saejin@192.168.1.5>
2014-12-25 22:14:17 -0500

    restructuring for sprite engine

--- src/assets/sprites/Sprite.java
+++ src/assets/sprites/Sprite.java
@@ -1,11 +1,55 @@
 package assets.sprites;
 
 import assets.textures.Texture2D;
+import org.lwjgl.util.vector.Vector2f;
 
 public class Sprite {
     
     private Texture2D tex;
-    private float x,y,priority;
+    private Vector2f pos;
+    private float rotation,priority;
+
+    public Sprite(Texture2D tex, float x, float y, float rotation, float priority) {
+        this.tex = tex;
+        pos = new Vector2f(x,y);
+        rotate(rotation);
+        this.priority = priority;
+    }
+    
+    
+    
+    
+    
+    
+    public void rotate(float toRot) {
+        rotation+=toRot;
+        if(rotation >=360) rotation-=360;
+        else if (rotation < 0) rotation+=360;
+    }
+    
+    public void translate(float x, float y) {
+        pos.translate(x, y);
+    }
+
+    public Texture2D getTex() {
+        return tex;
+    }
+
+    public float getX() {
+        return pos.x;
+    }
+
+    public float getY() {
+        return pos.y;
+    }
+
+    public float getPriority() {
+        return priority;
+    }
+
+    public void setPriority(float priority) {
+        this.priority = priority;
+    }
     
     
     
